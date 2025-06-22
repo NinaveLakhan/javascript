283b2ea499cc9dc0f6e5f963e3bff3240bdd800e
NinaveLakhan <lakhanninave2003@gmail.com>
2025-06-22 16:47:08 +0000

    conversionofDatatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "hitesh"
+
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "lakhan" => true
+
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+console.log(typeof stringNumber);

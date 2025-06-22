3315e4f9c08154a747a1fc249a2f5b347fbfe19c
NinaveLakhan <lakhanninave2003@gmail.com>
2025-06-22 16:25:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";   // treat all JS code as newer version
+
+// alert( 3 + 3 ) // We are using Node_Js , not a browser
+
+console.log(3 
+    +
+     3)  // code readability should be high 
+
+console.log("Lakhan")
+
+let name = "Lakhan"
+let age = 21
+let isLoggedIn = true
+
+// number  => 2 to power 53
+// bigint  =>
+// string  => ""
+// boolean => true/false
+// null    => standalone value
+// undefined
+// symbol  => unique
+
+
+// object
+
+console.log(typeof undefined);   // it's type is undefined
+console.log(typeof null);   // it's type is object
